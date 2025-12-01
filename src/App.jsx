9762bced74b673af66f7c1bc59e94e98218b24e9
Sabin Ghimire <sabinghimire223@gmail.com>
2025-12-01 21:43:22 +0545

    Loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
+import authService from "./appWrite/auth"
 import './App.css'
+import {login, logout} from "./store/authSlice"
 
 function App() {
+const [loading, setLoading]= useState(true);
+const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-    <h1>Blog With AppWrite</h1>
-    </>
-  )
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if (userData) {dispatch(login({userData}))}
+    else { dispatch(dispatch(logout()))}
+  })
+  .finally(()=> setLoading(false))
+},[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+<div className='w-full block'>
+  <Header />
+  <main>
+    {/* <Outlet /> */}
+  </main>
+  <Footer />
+</div>
+    </div>
+  ) ()
 }
 
 export default App
