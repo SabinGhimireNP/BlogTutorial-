ce0af2d6f4b5abeedd0cb1a1eca8b4f5b3b84b91
Sabin Ghimire <sabinghimire223@gmail.com>
2025-12-06 18:31:32 +0545

    Changes and Header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import { logoutButton, Container, Logo } from "../index"
+import {Link, link} from "react-router-dom"
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const authStatus = useSelector((state)=>state.auth.status)
+ const navigate = useNavigate() 
+
+ const navItems = [
+{
+  name:"Home",
+  slug:"/",
+  active:true
+},
+{
+  name:"Login",
+  slug:"/login",
+  active:!authStatus,
+},
+{
+  name:"SignUp",
+  slug: "/signup",
+  active: !authStatus,
+},
+{
+  name:"All Posts",
+  slug:"/all-posts",
+  active: authStatus,
+},
+{
+  name:"Add Posts",
+  slug:"/add-posts",
+  active: authStatus,
+}
+ ]
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+       <nav className='flex'>
+        <div className="mr-4">
+          <Link to="/"> 
+          <Logo width='70px'/>
+          </Link>
+          <ul className='flex ml-auto'>
+            {navItems.map((Items)=>{ Items.active ? (
+              <li key={Items.name}>
+                <button onClick={()=>navigate(Items.slug)} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>{Items.name}</button>
+              </li>
+            ):null}
+            
+          )}
+          {authStatus && (<li>
+            <logoutButton />
+            </li>
+          ) }
+          </ul>
+        </div>
+        </nav> 
+      </Container>
+    </header>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import logoutButton from "./logoutButton";
+import Container from "./Container/Container";
+import Logo from "./Logo";
 
-export { 
-    Header,
-     Footer
-
- };
+export { Header, Footer, logoutButton, Container, Logo };
