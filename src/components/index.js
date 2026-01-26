b0c29f824d1d1b997e660f0664cff0b058aa50d6
SabinGhimireNP <sabinghimire223@gmail.com>
2026-01-26 09:53:33 +0545

    Post Form method

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import logoutButton from "./logoutButton";
 import Container from "./Container/Container";
 import Logo from "./Logo";
+import RTE from "./RTE";
+import Input from "./Input/Input";
+import Select from "./Select/Select";
+import Button from "./button";
 
-export { Header, Footer, logoutButton, Container, Logo };
+export { Header, Footer, logoutButton, Container, Logo, RTE, Input, Select, Button };
